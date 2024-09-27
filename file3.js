69f63d175978493a2d6b7c28e9f79916f586f334
Amit Raj <rajamitnine@gmail.com>
2024-09-28 01:01:07 +0530

    Create a null dataType and log its type into the console

--- file3.js
+++ file3.js
@@ -5,3 +5,11 @@ console.log(typeof(firstName));
 
 firstName = "Harshit";
 console.log(typeof(firstName), firstName);
+
+// null :
+
+let myName = null;
+console.log(myName, typeof(myName));
+
+myName = "Mohit";
+console.log(myName, typeof(myName));
