62699dc3527dd28ad25a23d0a7943e7a93888590
Amit Raj <rajamitnine@gmail.com>
2024-09-27 23:41:14 +0530

    Access the elements of a string using their index, find length of a string using string.length property

---
+++ file2.js
@@ -0,0 +1,13 @@
+// String Indexing :
+
+let firstName = "Amit";
+console.log(firstName[3]);
+
+// Finding the length of a 'string' :
+console.log(firstName.length);
+
+// Accessing the last element of a string :
+console.log(firstName[firstName.length-1]);
+
+// Accessing the second-last element of a string :
+console.log(firstName[firstName.length-2]);
