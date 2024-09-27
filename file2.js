4c7e10bd5d02231f1391117bfad61bdc86afa07f
Amit Raj <rajamitnine@gmail.com>
2024-09-28 00:11:34 +0530

    Add some in-built methods of strings like 'trim()', 'toUpperCase()', 'toLowerCase()'

--- file2.js
+++ file2.js
@@ -11,3 +11,23 @@ console.log(firstName[firstName.length-1]);
 
 // Accessing the second-last element of a string :
 console.log(firstName[firstName.length-2]);
+
+// Some in-built methods for string :
+// Strings are immutable, so every method will give a brand new String.
+
+// trim() method :
+let lastName = "    Raj    ";
+console.log(lastName.length);
+console.log(lastName);
+
+let trimmedString = lastName.trim();
+console.log(trimmedString.length);
+console.log(trimmedString);
+
+// toUpperCase() method :
+let upperCaseString = trimmedString.toUpperCase();
+console.log(upperCaseString);
+
+// toLowerCase() method :
+let lowerCaseString = upperCaseString.toLowerCase();
+console.log(lowerCaseString);
