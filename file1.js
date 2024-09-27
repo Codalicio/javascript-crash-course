c2cf55d0873e9297084a0b7fd71adf96c2e7aa85
Amit Raj <rajamitnine@gmail.com>
2024-09-27 21:23:34 +0530

    Add a variable, use it and then change it's value

--- file1.js
+++ file1.js
@@ -1,4 +1,21 @@
+"use strict";
+
 console.log("Hello World!");
 
 // Backticks around a string :
 console.log(`My name is Amit Raj.`);
+
+// Intro to Variables :
+
+// Variables are used to store information/data that can be used and modified later anytime.
+// Variable names are case sensitive.
+
+// Declaring a Variable :
+var firstName = "Amit";
+
+// Using a variable :
+console.log(firstName);
+
+// Changing value of the variable :
+firstName = "Sonu";
+console.log(firstName);
