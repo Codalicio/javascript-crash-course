9da8f5b0ead1cd16df7a901793080a4923a86b28
Amit Raj <rajamitnine@gmail.com>
2024-09-28 15:36:51 +0530

    Add BigInt Primitive Data Type with its application

--- file3.js
+++ file3.js
@@ -13,3 +13,16 @@ console.log(myName, typeof(myName));
 
 myName = "Mohit";
 console.log(myName, typeof(myName));
+
+// Big Int :
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+let num1 = BigInt(1234);
+let num2 = 12n;
+
+console.log(num1, typeof(num1));
+console.log(num2, typeof(num2));
+
+let num3 = num1 + num2;
+console.log(num3, typeof(num3));
