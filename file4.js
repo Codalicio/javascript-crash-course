e4b2b027798991dcd3df24231865731c21058a60
Amit Raj <rajamitnine@gmail.com>
2024-09-28 18:40:29 +0530

    Add switch statement

--- file4.js
+++ file4.js
@@ -25,3 +25,32 @@ if(temperature < 0) {
 } else {
     console.log("It's too hot outside!");
 };
+
+// switch statement :
+let day = 3;
+
+switch(day) {
+    case 0 :
+        console.log("Sunday");
+        break;
+    case 1 :
+        console.log("Monday");
+        break;
+    case 2 :
+        console.log("Tuesday");
+        break;
+    case 3 :
+        console.log("Wednesday");
+        break;
+    case 4 :
+        console.log("Thursday");
+        break;
+    case 5 :
+        console.log("Friday");
+        break;
+    case 6 :
+        console.log("Saturday");
+        break;
+    default :
+        console.log("Invalid day!");
+};
