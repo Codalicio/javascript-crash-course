c0fc82424afab8fdfe9f5805623e1d564b182ea0
Amit Raj <rajamitnine@gmail.com>
2024-09-29 21:02:33 +0530

    Concat an array with an empty array to get an independent copy of that array

--- file6.js
+++ file6.js
@@ -19,3 +19,13 @@ arr4 = ['item1', 'item2'];
 arr5 = arr4.slice(0);
 console.log(arr4, arr5, arr4 === arr5);
 
+// Array concatenation :
+
+arr6 = ['obj1', 'obj2'];
+arr7 = [].concat(arr6);
+
+console.log(arr6, arr7, arr6 === arr7);
+
+arr6.push('obj67');
+arr7.push('obj3');
+console.log(arr6, arr7);
