5c192fe3ceb004af95679c522d4c2eec510a341d
Amit Raj <rajamitnine@gmail.com>
2024-09-30 17:41:38 +0530

    Use const for creating an array

---
+++ file8.js
@@ -0,0 +1,10 @@
+// Using const for creating an array :
+
+const fruits = ["apple", "pineapple", "mango"];
+fruits.push("banana");
+console.log(fruits);
+removedFruit = fruits.shift();
+console.log(removedFruit);
+console.log(fruits);
+fruits.unshift("strawberry");
+console.log(fruits);
