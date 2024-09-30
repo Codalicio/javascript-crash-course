9cb3284d7365fa455758c6e9d6f48d00c0e75240
Amit Raj <rajamitnine@gmail.com>
2024-09-30 23:16:52 +0530

    Create objects and run 'for of' and 'for in' loop on them

---
+++ file13.js
@@ -0,0 +1,27 @@
+// Arrays are good but not sufficient for real-world data.
+
+// Objects :
+// Objects store data in 'key:value' pairs.
+// Objects don't have index and thus, can't be retrieved through index.
+
+// Create objects :
+
+const personDetails = {
+    name : 'Amit',
+    age : 27,
+    address : "Hajipur, Bihar-844101",
+    hobbies : ['guitar', 'sleeping', 'listening to music']
+};
+console.log(personDetails, typeof(personDetails));
+console.log(personDetails.name);
+console.log(personDetails.age);
+console.log(personDetails.address);
+
+let hobbies = personDetails.hobbies;
+for(let hobby of hobbies) {
+    console.log(hobby);
+};
+
+for(let index in hobbies) {
+    console.log(hobbies[index].toUpperCase());
+};
