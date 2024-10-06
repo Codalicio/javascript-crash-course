cf85336229577462bca3611e0f224ce04cb5e86d
Amit Raj <rajamitnine@gmail.com>
2024-10-07 04:17:03 +0530

    Perform operations with different scenarios to understand 'Hoisting'

---
+++ file24.js
@@ -0,0 +1,33 @@
+// Hoisting :
+
+// Function Declaration Case :
+// hello();
+// function hello() {
+//     console.log("Hello World!");
+// };
+
+// Function Expression Case :
+// hello();
+// const hello = function() {
+//     console.log("Hello World!");
+// };
+
+// Arrow Function Case:
+// hello();
+// const hello = () => {
+//     console.log("Hello World!");
+// };
+
+// Variables Case :
+
+// console.log(var1);
+// const var1 = "hello";
+
+// console.log(var1);
+// let var1 = "Hello";
+
+console.log(var1);
+var var1 = "hello world!";
+
+// -----------------------------------------------------------
+
