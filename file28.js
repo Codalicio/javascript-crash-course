799c16d082063c1e283f0f84e1d48dad536b960d
Amit Raj <rajamitnine@gmail.com>
2024-10-07 05:22:03 +0530

    Feed default parameters in a function

---
+++ file28.js
@@ -0,0 +1,12 @@
+// Default Parameters :
+
+const sum = (num1, num2=7) => num1 + num2;
+
+const ans = sum(9);
+console.log(ans);
+
+const ans1 = sum(89, 89);
+console.log(ans1);
+
+// -------------------------------------------------------
+
